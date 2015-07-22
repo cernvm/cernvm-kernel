37756102fbb799b310396be26c6525858e3c21c5
Jakob Blomer <jblomer@cern.ch>
2015-07-22 11:37:26 +0200

    change aufs git url

--- params.mk
+++ params.mk
@@ -7,7 +7,7 @@ AFS_TARBALL = openafs-$(AFS_VERSION)-src.tar.bz2
 AFS_URL = http://www.openafs.org/dl/openafs/$(AFS_VERSION)/$(AFS_TARBALL)
 
 AUFS_BRANCH = aufs3.18.1+
-AUFS_GIT = git://git.code.sf.net/p/aufs/aufs3-standalone
+AUFS_GIT = https://github.com/cernvm/aufs3-standalone.git
 
 VBOX_VERSION = 4.3.28
 VBOX_ISO = VBoxGuestAdditions_$(VBOX_VERSION).iso

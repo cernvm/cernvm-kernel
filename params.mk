9c2a133c257457ac9a7dfb4f0f86e6895b1099db
Jakob Blomer <jblomer@cern.ch>
2018-02-20 13:31:31 +0100

    update versions

--- params.mk
+++ params.mk
@@ -25,13 +25,13 @@ BUILD = $(TOP)/build-$(CVM_KERNEL_VERSION)
 SRC = $(TOP)/src-$(CVM_KERNEL_VERSION)
 KERN_DIR = $(BUILD)/linux-$(LINUX_VERSION)
 
-LINUX_VERSION = 4.14.14
+LINUX_VERSION = 4.14.18
 LINUX_TARBALL = linux-$(LINUX_VERSION).tar.xz
 LINUX_URL = https://www.kernel.org/pub/linux/kernel/v4.x/$(LINUX_TARBALL)
 
 AUFS_BRANCH = aufs4.14
 AUFS_GIT = https://github.com/cernvm/aufs4-standalone.git
 
-VBOX_VERSION = 5.2.2
+VBOX_VERSION = 5.2.6
 VBOX_ISO = VBoxGuestAdditions_$(VBOX_VERSION).iso
 VBOX_URL = http://download.virtualbox.org/virtualbox/$(VBOX_VERSION)/$(VBOX_ISO)
